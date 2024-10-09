b3591acbdc740a41692fe94778d437cf8e6d35f8
heecheol153 <heecheol153@gmail.com>
2024-10-09 20:05:32 +0900

    #1.3 Hello World

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Hello flutter!'),
+        ),
+        body: Center(
+          child: Text('Hello world!'),
+        ),
+      ),
+    );
+  }
+}
