e367e02b105d24fbbfec84b804be35fcaf8a34dd
heecheol153 <heecheol153@gmail.com>
2024-10-16 20:53:04 +0900

    2.5 Reusable Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/Button.dart';
 
 void main() {
   runApp(App());
@@ -12,7 +13,7 @@ class App extends StatelessWidget {
         backgroundColor: const Color(0xFF181818),
         body: Padding(
           padding: const EdgeInsets.symmetric(
-            horizontal: 40,
+            horizontal: 20, //40->20 Button 폭 넓이 에러 발생
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -20,13 +21,13 @@ class App extends StatelessWidget {
               const SizedBox(
                 height: 80,
               ),
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      const Text(
+                      Text(
                         'Hey, Selena',
                         style: TextStyle(
                           color: Colors.white,
@@ -37,7 +38,7 @@ class App extends StatelessWidget {
                       Text(
                         'Welcome back',
                         style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
                           fontSize: 18,
                         ),
                       ),
@@ -51,7 +52,9 @@ class App extends StatelessWidget {
               Text(
                 'Total Balance',
                 style: TextStyle(
-                    fontSize: 22, color: Colors.white.withOpacity(0.8)),
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
               ),
               const SizedBox(
                 height: 5,
@@ -67,27 +70,21 @@ class App extends StatelessWidget {
               const SizedBox(
                 height: 30,
               ),
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(45)),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 50,
-                      ),
-                      child: Text(
-                        'Transfer',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  )
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Button(
+                    text: 'Transfer',
+                    bgColor: Color(0xFFF1B33B),
+                    textColor: Colors.black,
+                  ),
+                  Button(
+                    text: 'Request',
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white,
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
