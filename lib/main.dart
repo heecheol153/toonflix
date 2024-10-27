5f951ae8f0e858c928deb0e457a59648ad708b80
heecheol153 <heecheol153@gmail.com>
2024-10-27 23:25:32 +0900

    5.1 AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,14 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({super.key}); // 이위젯의key를 stateless widget라는 super class에 보내는것
+  // 위젯은 ID같은 식별자 역할을 하는key가 있다는것이다.Flutter가 위젯을 빠르게 찾을수있게 말이지.
+  // 명심할것은 key라는걸 가지고있고, ID처럼 쓰인다는것이다. 위젯을 식별하기위해서.
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFE7626C),
-        textTheme: const TextTheme(
-          headlineLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
