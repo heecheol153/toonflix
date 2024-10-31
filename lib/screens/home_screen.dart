f30af99bd1d5df1c23a8a0bd8cb4622d8465292c
heecheol153 <heecheol153@gmail.com>
2024-10-31 20:45:59 +0900

    5.13 Detail Info

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,7 +17,7 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: Colors.white,
           foregroundColor: Colors.green,
           title: const Text(
-            Widget.title,
+            "오늘의 웹툰",
             style: TextStyle(
               fontSize: 24,
             ),
